6c30b3a6370abb73a87dc212f1fa980f3aa788e8
CommandBlock6417 <pgkoutsoumanis@gmail.com>
2020-06-16 09:28:30 +0300

    Added test main

---
+++ main.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret stored in Heroku
+ 
+
+client.on('ready', () => {
+
+    console.log('Tangerine has started owo!');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.channel.send('pong');
+
+       }
+    if (message.content === 'crash') {
+
+       proces.exit();
+
+    }
+
+});
