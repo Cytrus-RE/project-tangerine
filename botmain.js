f495d4ff772477878f01792a2f06c0a9e33706a6
Odyssey346 <alej0hio2007@gmail.com>
2020-07-15 20:16:51 +0200

    Yo <@395990735934980097>, mind pulling changes real quick?
    Signed-off-by: Odyssey346 <alej0hio2007@gmail.com>

--- botmain.js
+++ botmain.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 
+
 const client = new Discord.Client({
     disableEveryone: true, // I'm fairly certain we don't want to ping everyone in a server.
     disabledEvents: ['TYPING_START'] // This disables the "is typing..." text under the messagebox in the Discord client
@@ -44,4 +45,16 @@ client.on('message', message => {
     }
 
 
+});
+
+client.on('message', message => {
+    var exec = require('child_process').exec, child;
+    if(message.content === 'pullchanges') {
+        await exec('git pull origin master')
+        // I'm confident that this will work. If it doesn't, well, I don't care, I'm not spending 10 hours getting git pulling to work
+        message.channel.send('Changes were pulled. Rebooting!')
+        await exec('node botmain.js')
+        setTimeout(5)
+        process.exit();
+    }
 });
